50a6f8b8ec01e8401ba305d8ee949fed0e28af8e
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2023-04-01 22:47:27 +0900

    Add files via upload

--- API-Info-Paises/testes/captar_indicadores.py
+++ API-Info-Paises/testes/captar_indicadores.py
@@ -6,8 +6,7 @@ def mostrar_id_dos_indicadores():
     request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/paises/indicadores")
     dicionario = json.loads(request.text)
     for i in dicionario:
-        print(f"*{i['id']}")
-        print(i['indicador'])
+        print(f"*({i['id']}):  {i['indicador']}")
 
 
 def mostrar_indicadores(pais, id):
@@ -21,6 +20,17 @@ def mostrar_indicadores(pais, id):
         print(dicionario_do_indicador[0][i])
 
 
+def mostrar_todos_os_indicadores_de_um_pais(pais):
+    pais = pais
+    request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/paises/{pais}/indicadores/")
+    dicionario_de_indicador = json.loads(request.text)
+
+    for i in dicionario_de_indicador:
+        print(f"{i['indicador']}: \n{i['series'][0]}")
+
+
 mostrar_id_dos_indicadores()
 print(f"\n{'-'*20}\n")
 mostrar_indicadores('US', '77849')
+print(f"\n//{'-'*20}\n")
+mostrar_todos_os_indicadores_de_um_pais("BR")
