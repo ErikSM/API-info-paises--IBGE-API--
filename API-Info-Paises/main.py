085a3ea84044c43c34d510646e741a793d4a92c8
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2023-04-29 22:08:43 +0900

    Add files via upload

--- API-Info-Paises/main.py
+++ API-Info-Paises/main.py
@@ -3,24 +3,41 @@ from tkinter import *
 import json
 import requests
 
+from actions import capture_indicators
+
+
+def start(window=None):
+    if window is not None:
+        window.destroy()
+    AppStart()
+
 
 class AppStart:
 
     def __init__(self):
 
-        self.window = Tk()
-        self.window.title("Busca de Informacoes Basicas de Paises")
-        self.window.geometry("+300+160")
-        self.window.minsize(height=200, width=200)
-        self.window.resizable(0, 0)
-
         self.country_select = None
 
+        self.country_open = None
+        self.sigla_of_open_country = None
+
+        self.window = Tk()
+        self.window.title("Busca de Informacoes Basicas de Paises")
+        self.window.geometry("+400+100")
+        self.window.minsize(height=200, width=200)
+        self.window.resizable(False, False)
+
+        # / Menus
         self.menu = Menu(self.window)
         self.window.config(menu=self.menu)
-        self.menu_start = Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Menu", menu=self.menu_start)
-        self.menu_start.add_command(label="Indice de Paises", command=self.index)
+
+        # // inicio
+        self.menu.add_command(label="Tela inicial", command=lambda: start(self.window))
+
+        # // incides
+        self.menu_indices = Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="Indices", menu=self.menu_indices)
+        self.menu_indices.add_command(label="Indice de Paises", command=self.index)
 
         self.frame = Frame(self.window, bg="grey90")
         self.frame.pack()
@@ -32,16 +49,13 @@ class AppStart:
         self.sub_frame_right = Frame(self.frame, bg="grey90")
         self.sub_frame_right.pack(side="right")
 
-        self.label_text = Label(self.sub_frame_left, bg="grey90",
-                                text="(Consulte o Menu acima) e\ndigite a sigla do pais desejado:", font="Arial 7 bold")
-        self.label_text.grid(row=0, column=0, columnspan=2)
+        self.label_of_search = None
+        self.entry_of_search = None
+        self.button_search = None
+        self.label_of_list = None
+        self.initial_settings()
 
-        self.entry_place = Entry(self.sub_frame_left, font="arial 12", width=10, bd=3)
-        self.entry_place.grid(row=1, column=0)
-
-        self.button_search = Button(self.sub_frame_left, text="Search", font="Consolas 8 bold", bd=1,
-                                    command=self.search)
-        self.button_search.grid(row=1, column=1)
+        self.button_return = None
 
         self.list = Listbox(self.sub_frame_right, width=90, height=9, bd=2, bg="grey90")
         self.list.pack(fill="both")
@@ -49,12 +63,41 @@ class AppStart:
 
         self.window.mainloop()
 
+    def initial_settings(self):
+        self.label_of_search = Label(self.sub_frame_left, font="Arial 7 bold", bg="grey90")
+        self.label_of_search.configure(text="(Consulte o Menu indice acima) e\ndigite a sigla do pais desejado:")
+        self.label_of_search.grid(row=0, column=0, columnspan=2)
+
+        self.entry_of_search = Entry(self.sub_frame_left, font="arial 12", width=10, bd=3)
+        self.entry_of_search.grid(row=1, column=0)
+
+        self.button_search = Button(self.sub_frame_left)
+        self.button_search.configure(font="Consolas 8 bold", text="Busca", bd=1, command=self.search)
+        self.button_search.grid(row=1, column=1)
+
+        self.label_of_list = Label(self.sub_frame_right)
+        self.label_of_list.configure(text="-----------   ----------- :", bg="grey90")
+        self.label_of_list.pack()
+
+    def back_to_search_settings(self):
+        self.button_return.destroy()
+        self.initial_settings()
+        self.entry_of_search.insert(0, self.country_select)
+        self.search()
+
     def search(self):
         self.list.config(state=tkinter.NORMAL)
 
-        self.country_select = self.entry_place.get().upper()
+        self.country_select = self.entry_of_search.get().upper()
 
-        try:
+        if len(self.country_select) == 0:
+            self.text.delete(1.0, END)
+            self.list.delete(0, END)
+
+            self.list.insert(END, f"   {'X' * 10} ")
+            self.list.insert(END, "   %##$  Informacoes nao encontradas ou Imprecisas   #$%& ")
+            self.text.insert(1.0, f"\n\n{'X' * 10}\n\n      #$%& ErRor #$%& ")
+        else:
             request = requests.get(f""
                                    f"https://servicodados.ibge.gov.br/api/v1/paises/"
                                    f"{self.country_select}"
@@ -64,10 +107,12 @@ class AppStart:
 
             self.text.delete(1.0, END)
             self.list.delete(0, END)
-            self.entry_place.delete(0, END)
+            self.entry_of_search.delete(0, END)
 
             self.text.config(bg="grey70")
             self.list.config(bg="grey70")
+
+            self.label_of_list.config(text=" Informacoes basicas:")
             for item in dict_of_search:
                 self.list.insert(END, f'Pais: {item["nome"]["abreviado"]}')
                 self.list.insert(END, f'Area: {item["area"]["total"]}{item["area"]["unidade"]["símbolo"]}')
@@ -77,15 +122,23 @@ class AppStart:
                 self.list.insert(END, f'Unidade monetaria: {item["unidades-monetarias"][0]["nome"]}')
 
                 self.text.insert(END, f'Historico: {item["historico"]}')
-        except Exception as ex:
-            self.list.insert(END, "   %##$  Informacoes nao encontradas ou Imprecisas   #$%& ")
-            self.text.insert(1.0, f"{ex}\n\n      #$%& ErRor #$%& ")
+
+                self.country_open = item["nome"]["abreviado"]
+                self.country_open = self.country_open.lower()
+
+            self.sigla_of_open_country = self.country_select
+
+            self.button_search.config(text=f"<Informacoes avancadas>")
+            self.button_search.configure(command=self.advanced_information_about_country)
+            self.label_of_search.config(text=f"Clique para outros indicadores\nsobre:({self.country_open.title()})")
+            self.entry_of_search.destroy()
 
     def index(self):
-
         self.list.config(state=tkinter.NORMAL)
         request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/localidades/paises")
         dict_of_indexes = json.loads(request.text)
+
+        self.label_of_list.config(text=" Confira abaixo as siglas dos paises:")
 
         self.list.insert(END, "...")
         self.list.delete(0, END)
@@ -94,7 +147,7 @@ class AppStart:
         self.text.insert(END, "...")
         self.text.delete(1.0, END)
 
-        self.entry_place.delete(0, END)
+        self.entry_of_search.delete(0, END)
         self.text.config(bg="grey90")
         self.list.config(bg="grey70")
 
@@ -104,5 +157,61 @@ class AppStart:
 
         self.list.config(state=tkinter.DISABLED)
 
+    def advanced_information_about_country(self):
+        all_indicators = capture_indicators.show_indicators_id()
+        country_indicators = capture_indicators.show_all_indicators_of_selected_country(self.sigla_of_open_country)
 
-AppStart()
+        self.label_of_list.config(text=f"Informacoes detalhadas sobre: ({self.country_open.title()}):")
+        self.label_of_search.config(text="Busque pelo item selecionado:")
+        self.button_search.config(text=f"Especificar indicador")
+        self.button_search.configure(command=self.show_selected_indicator)
+
+        self.list.config(state=tkinter.NORMAL)
+
+        self.list.delete(0, END)
+        self.text.delete(1.0, END)
+
+        for i in all_indicators:
+            indicator = i
+            indicator_code = all_indicators[i]
+            self.list.insert(END, f"{indicator_code}:  {indicator}")
+
+        for i in country_indicators:
+            indicator = f"({i['id']}){i['indicador']}: {i['series'][0]['pais']}"
+            information = f"{i['series'][0]['serie']}"
+            self.text.insert(END, f"{indicator}\n")
+            self.text.insert(END, f"{information}\n\n")
+
+    def show_selected_indicator(self):
+        country = self.sigla_of_open_country
+        indicator = self.list.get(tkinter.ANCHOR)
+        indicator = indicator[0:5]
+        indicator_content = capture_indicators.show_specific_indicator_of_specific_country(country, indicator)
+
+        if len(indicator) == 0:
+            self.text.delete(1.0, END)
+            self.text.insert(END, f"Error\n\n{'X' * 10}\n\n Selecione uma opcao acima")
+        else:
+            self.list.delete(0, END)
+            self.text.delete(1.0, END)
+
+            self.list.insert(END, f"    **  {self.country_open.title()}({self.sigla_of_open_country})  **")
+            self.list.insert(END, f" ")
+            for i in indicator_content[0]:
+                if i == "series":
+                    for option in indicator_content[0][i][0]["serie"]:
+                        self.text.insert(END, f"**{option}\n")
+                else:
+                    self.list.insert(END, f'-{i.upper()}:   {indicator_content[0][i]}')
+                    self.list.insert(END, f" ")
+
+            self.button_search.destroy()
+            self.label_of_search.config(text=f"")
+
+            self.button_return = Button(self.sub_frame_left)
+            self.button_return.configure(font="Consolas 8 bold", text="voltar", bd=1,
+                                         command=self.back_to_search_settings)
+            self.button_return.grid(row=0, column=0)
+
+
+start()
