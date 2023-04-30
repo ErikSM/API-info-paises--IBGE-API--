2c348081bf5dc85aa3a83efda45bd29af73ff7f0
Erik Miyajima <50684425+ErikSM@users.noreply.github.com>
2023-04-30 21:48:32 +0900

    Add files via upload

--- API-Info-Paises/actions/capture_continents.py
+++ API-Info-Paises/actions/capture_continents.py
@@ -3,48 +3,41 @@ import requests
 
 
 request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/localidades/paises")
-dicionario_fonte = json.loads(request.text)
+source_dictionary = json.loads(request.text)
 
-regioes = dict()
+region = dict()
 
-for iten in dicionario_fonte:
-    regioes[iten['sub-regiao']['regiao']['nome']] = list()
+for item in source_dictionary:
+    region[item['sub-regiao']['regiao']['nome']] = list()
 
-for iten in dicionario_fonte:
-    if iten['sub-regiao']['regiao']['nome'] == "Ásia":
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
-    elif iten['sub-regiao']['regiao']['nome'] == "África":
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
-    elif iten['sub-regiao']['regiao']['nome'] == "América":
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
-    elif iten['sub-regiao']['regiao']['nome'] == "Europa":
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
-    elif iten['sub-regiao']['regiao']['nome'] == "Oceania":
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
+for item in source_dictionary:
+    if item['sub-regiao']['regiao']['nome'] == "Ásia":
+        region[item['sub-regiao']['regiao']['nome']].append(f"{item['id']['ISO-ALPHA-2']}: {item['nome']}")
+    elif item['sub-regiao']['regiao']['nome'] == "África":
+        region[item['sub-regiao']['regiao']['nome']].append(f"{item['id']['ISO-ALPHA-2']}: {item['nome']}")
+    elif item['sub-regiao']['regiao']['nome'] == "América":
+        region[item['sub-regiao']['regiao']['nome']].append(f"{item['id']['ISO-ALPHA-2']}: {item['nome']}")
+    elif item['sub-regiao']['regiao']['nome'] == "Europa":
+        region[item['sub-regiao']['regiao']['nome']].append(f"{item['id']['ISO-ALPHA-2']}: {item['nome']}")
+    elif item['sub-regiao']['regiao']['nome'] == "Oceania":
+        region[item['sub-regiao']['regiao']['nome']].append(f"{item['id']['ISO-ALPHA-2']}: {item['nome']}")
     else:
-        regioes[iten['sub-regiao']['regiao']['nome']].append(iten['nome'])
+        region[item['sub-regiao']['regiao']['nome']].append(item['nome'])
 
 
-def mostrar_continentes_existentes():
-    continentes = list()
-    for i in regioes:
-        continentes.append(i)
-    return continentes
+def show_all_continents():
+    continents = list()
+    for i in region:
+        continents.append(i)
+    return continents
 
 
-def mostrar_paises_de_cada_continente():
-    return regioes
+def show_contries_each_continent():
+    return region
 
 
-def mostrar_numero_de_paises_de_cada_continente():
-    dicionario_temporario = dict()
-    for i in regioes:
-        dicionario_temporario[i] = len(regioes[i])
-    return dicionario_temporario
-
-
-print(mostrar_paises_de_cada_continente())
-print("--------------------------")
-print(mostrar_numero_de_paises_de_cada_continente())
-print("-------------------------")
-print(mostrar_continentes_existentes())
+def show_number_of_countries_from_each_continent():
+    number_of_contries = dict()
+    for i in region:
+        number_of_contries[i] = len(region[i])
+    return number_of_contries
--- API-Info-Paises/main.py
+++ API-Info-Paises/main.py
@@ -34,15 +34,15 @@ class AppStart:
         # // inicio
         self.menu.add_command(label="Tela inicial", command=lambda: start(self.window))
 
-        # // incides
-        self.menu_indices = Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Indices", menu=self.menu_indices)
-        self.menu_indices.add_command(label="Indice de Paises", command=self.index)
+        # // paises
+        self.menu_countries = Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="Paises", menu=self.menu_countries)
+        self.menu_countries.add_command(label="Indice de Paises", command=self.open_countries)
 
         # // continentes(em desenvolvimento)
-        self.menu_continentes = Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Continentes", menu=self.menu_continentes)
-        self.menu_continentes.add_command(label="Sobre continentes", command=self.abrir_continentes)
+        self.menu_continents = Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="Continentes", menu=self.menu_continents)
+        self.menu_continents.add_command(label="Indice de Continentes", command=self.open_continents)
 
         self.frame = Frame(self.window, bg="grey90")
         self.frame.pack()
@@ -58,7 +58,7 @@ class AppStart:
         self.entry_of_search = None
         self.button_search = None
         self.label_of_list = None
-        self.initial_settings()
+        self._initial_settings()
 
         self.button_return = None
 
@@ -68,7 +68,7 @@ class AppStart:
 
         self.window.mainloop()
 
-    def initial_settings(self):
+    def _initial_settings(self):
         self.label_of_search = Label(self.sub_frame_left, font="Arial 7 bold", bg="grey90")
         self.label_of_search.configure(text="(Consulte o Menu indice acima) e\ndigite a sigla do pais desejado:")
         self.label_of_search.grid(row=0, column=0, columnspan=2)
@@ -84,16 +84,25 @@ class AppStart:
         self.label_of_list.configure(text="-----------   ----------- :", bg="grey90")
         self.label_of_list.pack()
 
-    def back_to_search_settings(self):
+    def _back_to_search_settings(self):
         self.button_return.destroy()
-        self.initial_settings()
+        self._initial_settings()
         self.entry_of_search.insert(0, self.country_select)
         self.search()
 
-    def search(self):
+    def search(self, from_continent=False):
         self.list.config(state=tkinter.NORMAL)
 
-        self.country_select = self.entry_of_search.get().upper()
+        select_from_list = self.list.get(tkinter.ANCHOR)
+
+        if not from_continent:
+            if len(select_from_list) == 0:
+                self.country_select = self.entry_of_search.get().upper()
+            else:
+                self.country_select = select_from_list[0:2]
+        else:
+            select_from_list = self.list.get(tkinter.ANCHOR)
+            self.country_select = select_from_list[0:2]
 
         if len(self.country_select) == 0:
             self.text.delete(1.0, END)
@@ -134,42 +143,18 @@ class AppStart:
             self.sigla_of_open_country = self.country_select
 
             self.button_search.config(text=f"<Informacoes avancadas>")
-            self.button_search.configure(command=self.advanced_information_about_country)
+            self.button_search.configure(command=self._advanced_information_about_country)
             self.label_of_search.config(text=f"Clique para outros indicadores\nsobre:({self.country_open.title()})")
             self.entry_of_search.destroy()
 
-    def index(self):
-        self.list.config(state=tkinter.NORMAL)
-        request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/localidades/paises")
-        dict_of_indexes = json.loads(request.text)
-
-        self.label_of_list.config(text=" Confira abaixo as siglas dos paises:")
-
-        self.list.insert(END, "...")
-        self.list.delete(0, END)
-        self.list.insert(END, f"    {'-' * 1}  (( Indice de Paises )) {'-' * 1}   \n ")
-
-        self.text.insert(END, "...")
-        self.text.delete(1.0, END)
-
-        self.entry_of_search.delete(0, END)
-        self.text.config(bg="grey90")
-        self.list.config(bg="grey70")
-
-        for country in dict_of_indexes:
-            self.list.insert(END, f"Sigla: {country['id']['ISO-ALPHA-2']}{' ' * 15}"
-                                  f"Estado: {country['nome']}{' ' * 2}\n")
-
-        self.list.config(state=tkinter.DISABLED)
-
-    def advanced_information_about_country(self):
+    def _advanced_information_about_country(self):
         all_indicators = capture_indicators.show_indicators_id()
         country_indicators = capture_indicators.show_all_indicators_of_selected_country(self.sigla_of_open_country)
 
         self.label_of_list.config(text=f"Informacoes detalhadas sobre: ({self.country_open.title()}):")
         self.label_of_search.config(text="Busque pelo item selecionado:")
         self.button_search.config(text=f"Especificar indicador")
-        self.button_search.configure(command=self.show_selected_indicator)
+        self.button_search.configure(command=self._show_selected_indicator)
 
         self.list.config(state=tkinter.NORMAL)
 
@@ -187,7 +172,7 @@ class AppStart:
             self.text.insert(END, f"{indicator}\n")
             self.text.insert(END, f"{information}\n\n")
 
-    def show_selected_indicator(self):
+    def _show_selected_indicator(self):
         country = self.sigla_of_open_country
         indicator = self.list.get(tkinter.ANCHOR)
         indicator = indicator[0:5]
@@ -215,35 +200,73 @@ class AppStart:
 
             self.button_return = Button(self.sub_frame_left)
             self.button_return.configure(font="Consolas 8 bold", text="voltar", bd=1,
-                                         command=self.back_to_search_settings)
+                                         command=self._back_to_search_settings)
             self.button_return.grid(row=0, column=0)
 
-    # //// ---  em desenvolvimento ----------  -------------------  -----------
-    def abrir_continentes(self):
-        continentes_existentes = capture_continents.mostrar_continentes_existentes()
-        mostrar_paises = capture_continents.mostrar_paises_de_cada_continente()
-        quantidade_de_paises = capture_continents.mostrar_numero_de_paises_de_cada_continente()
+    def open_countries(self):
+        self.list.config(state=tkinter.NORMAL)
+        request = requests.get(f"https://servicodados.ibge.gov.br/api/v1/localidades/paises")
+        dict_of_indexes = json.loads(request.text)
+
+        self.label_of_list.config(text=" Confira abaixo as siglas dos paises:")
+
+        self.list.delete(0, END)
+        self.text.delete(1.0, END)
+        self.entry_of_search.delete(0, END)
+
+        self.text.config(bg="grey90")
+        self.list.config(bg="grey70")
+
+        for country in dict_of_indexes:
+            self.list.insert(END, f"{country['id']['ISO-ALPHA-2']}:"
+                                  f" {country['nome']}{' ' * 2}\n")
+
+    def open_continents(self):
+        all_continents = capture_continents.show_all_continents()
+        all_countries_of_one_continent = capture_continents.show_contries_each_continent()
+        number_of_contries_from_each = capture_continents.show_number_of_countries_from_each_continent()
 
         self.list.config(state=NORMAL)
-        for i in continentes_existentes:
-            self.list.insert(END, f"{i}")
+        self.list.delete(0, END)
+        self.text.delete(1.0, END)
+
+        for continent in all_continents:
+            self.list.insert(END, f"{continent}")
 
         self.text.insert(END, f"Quantidade de paises:\n")
-        for i in quantidade_de_paises:
-            self.text.insert(END, f"-{i}: {quantidade_de_paises[i]} paises\n")
+        for continent in number_of_contries_from_each:
+            self.text.insert(END, f"-{continent}: {number_of_contries_from_each[continent]} paises\n")
 
         self.text.insert(END, f"\n\nPaises de cada continente:\n")
-        for i in mostrar_paises:
-            self.text.insert(END, f"* {i}:\n{mostrar_paises[i]}\n\n")
+        for continent in all_countries_of_one_continent:
+            self.text.insert(END, f"* {continent}:\n{all_countries_of_one_continent[continent]}\n\n")
+
+        self.label_of_list.config(text=f"Continentes:")
 
         self.button_search.config(text=f"Selecionar continente:")
-        self.button_search.configure(command=self.selecionar_continente)
+        self.button_search.configure(command=lambda: self._select_continent(
+            all_countries_of_one_continent, number_of_contries_from_each))
+
         self.label_of_search.config(text=f"Infomracoes sobre\ncontinente selecionado")
         self.entry_of_search.destroy()
 
-    def selecionar_continente(self):
-        pass
-    # ---------------------------------  ---------------------------   -----------
+    def _select_continent(self, mostrar_paises, quantidade_de_de_paises):
+        continente_selecionado = self.list.get(tkinter.ANCHOR)
+
+        self.text.delete(1.0, END)
+        self.text.insert(END, f"- {continente_selecionado}:\n")
+        self.text.insert(END, f"Este continente possui um total de "
+                              f"{quantidade_de_de_paises[continente_selecionado]} paises.")
+
+        self.list.delete(0, END)
+        for pais in mostrar_paises[continente_selecionado]:
+            self.list.insert(END, f"{pais}\n")
+
+        self.label_of_list.config(text=f"Paises pertencentes da: {continente_selecionado} ")
+        self.button_search.config(text=f"Busca sobre pais:")
+        self.button_search.configure(command=lambda: self.search(True))
+        self.label_of_search.config(text=f"Clique para infomracoes sobre\n o pais selecionado")
+        self.entry_of_search = Entry()
 
 
 start()
